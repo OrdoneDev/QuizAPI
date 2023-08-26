b30e4c3951d69c0131259fe7c92d82a3fa0b03fb
David Ordone <david9108@hotmail.com>
2023-08-26 10:16:22 -0300

    Criando controller e routes dos temas

--- api/controllers/TemaController.js
+++ api/controllers/TemaController.js
@@ -2,10 +2,35 @@ const database = require('../models')
 
 class TemaController {
     static async getAllTemas(_, res) {
-        try{
-
+        try {
+            const temas = await database.temas.findAll()
+            return res.status(200).json(temas)
         } catch(error) {
-            
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async getTemaById(req, res) {
+        const { id_tema } = req.params;
+
+        try {
+            const tema = await database.temas.findOne({ where: { id_tema: id_tema }})
+            return res.status(200).json(tema)
+        } catch(error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async getTemasByName(_, res) {
+        const { nome } = req.body
+
+        try {
+            const temas = await database.temas.findAll({
+                where: { titulo: { [Op.like]: `%${nome}%` }}
+            })
+            return res.status(200).json(temas)
+        } catch(error) {
+            return res.status(500).json(error.message)
         }
     }
 }
--- api/routes/temasRoute.js
+++ api/routes/temasRoute.js
@@ -3,6 +3,9 @@ const TemaController = require('../controllers/TemaController')
 
 const router = Router()
 
-//router.get()
+router
+    .get('/tema', TemaController.getAllTemas)
+    .get('/tema/:id_tema', TemaController.getTemaById)
+    .get('/tema/like/:nome', TemaController.getTemasByName)
 
 module.exports = router
